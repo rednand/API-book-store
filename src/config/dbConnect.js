26860571ede844c6c40dd945bfbb5b3b13adb6bc
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-04-18 14:02:23 -0300

    usando dotenv

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import "dotenv/config";
+
+const USER = process.env.DB_USER;
+const PASSWORD = process.env.DB_PASSWORD;
 
 mongoose.connect(
-  "mongodb+srv://rednand:10023@books.x7ro4.mongodb.net/bookStore"
+  `mongodb+srv://${USER}:${PASSWORD}@books.x7ro4.mongodb.net/bookStore`
 );
 
 let db = mongoose.connection;
